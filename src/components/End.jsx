bfc47ecbab4aca50ba3796e920df8facb91f6edb
jucetina <juliethcetina@gmail.com>
2023-08-15 15:43:17 -0500

    Se separan estilos de la vista

--- src/components/End.jsx
+++ src/components/End.jsx
@@ -1,35 +1,6 @@
 import check from "../assets/check.svg";
-import styled from "styled-components";
+import { Container, Icon, Text, Title } from "../css/End";
 
-const Container = styled.div`
-  max-width: 650px;
-  height: 568px;
-  padding: 0 100px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Icon = styled.img`
-  width: 80px;
-  height: 80px;
-  margin-bottom: 32px;
-`;
-
-const Title = styled.p`
-  font-size: 32px;
-  font-weight: 700;
-  margin-bottom: 14px;
-`;
-
-const Text = styled.p`
-  font-size: 16px;
-  font-weight: 400;
-  color: #9699aa;
-  text-align: center;
-  line-height: 25px;
-`;
 
 const End = () => {
   return (
--- src/css/End.js
+++ src/css/End.js
@@ -1,2 +1,32 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 650px;
+  height: 568px;
+  padding: 0 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Icon = styled.img`
+  width: 80px;
+  height: 80px;
+  margin-bottom: 32px;
+`;
+
+export const Title = styled.p`
+  font-size: 32px;
+  font-weight: 700;
+  margin-bottom: 14px;
+`;
+
+export const Text = styled.p`
+  font-size: 16px;
+  font-weight: 400;
+  color: #9699aa;
+  text-align: center;
+  line-height: 25px;
+`;
 
