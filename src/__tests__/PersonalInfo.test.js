2bfe6db972bcf0b6f5b4703006ecf87913ce3f27
jucetina <juliethcetina@gmail.com>
2023-08-17 19:22:44 -0500

    Se agregan mas test

--- src/__tests__/PersonalInfo.test.js
+++ src/__tests__/PersonalInfo.test.js
@@ -10,3 +10,22 @@ test('Renderiza componente PersonallInfo', () => {
   const title = screen.getByText(/please/i);
   expect(title).toBeInTheDocument();
 });
+
+test('Renderiza la selección del plan', () => {
+  const inputElement = screen.getAllByRole("textbox");
+  const submitButton = screen.getAllByRole("button", { name: /Next Step/i });
+
+  expect(inputElement).toHaveLength(3);
+  expect(submitButton).toHaveLength(2);
+
+  fireEvent.change(inputElement[0], { target: { value: "Juan"} } );
+  expect(inputElement[0].value).toBe("Juan");
+  fireEvent.change(inputElement[1], { target: { value: "juan@prueba.com"} } );
+  expect(inputElement[1].value).toBe("juan@prueba.com");
+  fireEvent.change(inputElement[2], { target: { value: "1234567"} } );
+  expect(inputElement[2].value).toBe("1234567");
+
+  fireEvent.click(submitButton[0]);
+
+  screen.debug();
+});
